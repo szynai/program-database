1aff1bdc24f1e571f7a86e5dfefb3532a10cdf5d
Mary Faith Salvador <salvadormaryfaith25@gmail.com>
2026-02-19 23:45:41 +0800

    final feature

--- MainSystem/src/a_package/Main.java
+++ MainSystem/src/a_package/Main.java
@@ -8,6 +8,60 @@ package a_package;
  *
  * @author redelyn
  */
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     
+    public static void main(String[] args) {
+        Repository repo = new Repository();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many students will you add? ");
+        int count = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("\n--- Student " + (i + 1) + " ---");
+
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("First Name: ");
+            String fname = sc.nextLine();
+
+            System.out.print("Last Name: ");
+            String lname = sc.nextLine();
+
+            System.out.print("Age: ");
+            int age = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("Course: ");
+            String course = sc.nextLine();
+
+            Student s = new Student.StudentBuilder()
+                    .ID(id)
+                    .FIRSTNAME(fname)
+                    .LASTNAME(lname)
+                    .AGE(age)
+                    .COURSE(course)
+                    .buildstudentBuilder();
+
+            repo.save(s);
+        }
+
+        System.out.println("\n=== MASTER LIST ===");
+        List<Student> list = repo.findAll();
+        for (Student s : list) {
+            System.out.println(s.getID() + " | " +
+                    s.getFIRSTNAME() + " " + s.getLASTNAME() +
+                    " | Age: " + s.getAGE() +
+                    " | Course: " + s.getCOURSE());
+        }
+
+        sc.close();
+    }
 }
+
--- MainSystem/src/a_package/Repository.java
+++ MainSystem/src/a_package/Repository.java
@@ -8,6 +8,86 @@ package a_package;
  *
  * @author redelyn
  */
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Repository {
-    
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/information";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Red123lyn45";
+
+    public Repository() {
+        createTable();
+    }
+
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    private void createTable() {
+        String sql = """
+            CREATE TABLE IF NOT EXISTS tbl_student (
+                id INT PRIMARY KEY,
+                firstName VARCHAR(100) NOT NULL,
+                lastName VARCHAR(100) NOT NULL,
+                age INT,
+                course VARCHAR(100)
+            );
+            """;
+
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void save(Student student) {
+        String sql = "INSERT INTO tbl_student(id, firstName, lastName, age, course) VALUES(?,?,?,?,?)";
+
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setInt(1, student.getID());
+            pstmt.setString(2, student.getFIRSTNAME());
+            pstmt.setString(3, student.getLASTNAME());
+            pstmt.setInt(4, student.getAGE());
+            pstmt.setString(5, student.getCOURSE());
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Student> findAll() {
+        List<Student> Students = new ArrayList<>();
+        String sql = "SELECT * FROM tbl_student;";
+
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                Student student = new Student.StudentBuilder()
+                        .ID(rs.getInt("id"))
+                        .FIRSTNAME(rs.getString("firstName"))
+                        .LASTNAME(rs.getString("lastName"))
+                        .AGE(rs.getInt("age"))
+                        .COURSE(rs.getString("course"))
+                        .buildstudentBuilder();
+
+                Students.add(student);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return Students;
+    }
 }
+
