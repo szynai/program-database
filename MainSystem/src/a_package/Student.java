910f5dada0e4dd4201ff681ded29e31a91d4f3d8
JuliaManeclang <juliamaneclang@gmail.com>
2026-02-19 23:37:43 +0800

    Update Student.java

--- MainSystem/src/a_package/Student.java
+++ MainSystem/src/a_package/Student.java
@@ -9,5 +9,63 @@ package a_package;
  * @author redelyn
  */
 public class Student {
+    private final int ID;
+    private final String FIRSTNAME;
+    private final String LASTNAME;
+    private final int AGE;
+    private final String COURSE;
+
+    private Student(StudentBuilder builder) {
+        this.ID = builder.ID;
+        this.FIRSTNAME = builder.FIRSTNAME;
+        this.LASTNAME = builder.LASTNAME;
+        this.AGE = builder.AGE;
+        this.COURSE = builder.COURSE;
+    }
+
+    public int getID() { return ID; }
+    public String getFIRSTNAME() { return FIRSTNAME; }
+    public String getLASTNAME() { return LASTNAME; }
+    public int getAGE() { return AGE; }
+    public String getCOURSE() { return COURSE; }
+
     
+    public static class StudentBuilder {
+
+        private int ID;
+        private String FIRSTNAME;
+        private String LASTNAME;
+        private int AGE;
+        private String COURSE;
+
+        public StudentBuilder ID(int id) {
+            this.ID = id;
+            return this;
+        }
+
+        public StudentBuilder FIRSTNAME(String firstName) {
+            this.FIRSTNAME = firstName;
+            return this;
+        }
+
+        public StudentBuilder LASTNAME(String lastName) {
+            this.LASTNAME = lastName;
+            return this;
+        }
+
+        public StudentBuilder AGE(int age) {
+            this.AGE = age;
+            return this;
+        }
+
+        public StudentBuilder COURSE(String course) {
+            this.COURSE = course;
+            return this;
+        }
+
+        
+          public Student buildstudentBuilder() {
+            return new Student(this);
+        }
+    }
 }
